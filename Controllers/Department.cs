e75bf4cfe2187f53956f58755a929eb9523f84d4
Brayanpj <brayanpj208@hotmail.com>
2022-11-07 21:45:47 -0300

    Project Create

---
+++ Controllers/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace SalesWebMvc.Controllers
+{
+    public class Department
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+    }
+}
---
+++ Controllers/DepartmentsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+         
+            return View();
+        }
+    }
+}
